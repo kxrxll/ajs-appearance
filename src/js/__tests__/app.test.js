f559b16648fb251a617a68b54e28437bb436338d
kxrxll <kovalyovkirill@gmail.com>
2021-08-14 01:52:30 +0300

    solved

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,7 +1,13 @@
 import Settings from '../app';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+test('straight test', () => {
+  const settings = new Settings();
+  // Вот в этом месте я понял что задание на выдержку в нейминге
+  expect([...settings.settings.keys()]).toEqual(['theme', 'music', 'difficulty']);
+});
 
-  expect(result).toBe(6);
+test('return test', () => {
+  const settings = new Settings();
+  // А тут я понял что у jest'а проблемы с геттерами
+  expect([...settings.get().keys()]).toEqual(['theme', 'music', 'difficulty']);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,18 @@
 class Settings {
-  
+  constructor(
+    theme = 'dark',
+    music = 'trance',
+    difficulty = 'easy',
+  ) {
+    this.settings = new Map();
+    this.settings.set('theme', theme);
+    this.settings.set('music', music);
+    this.settings.set('difficulty', difficulty);
+  }
+
+  get() {
+    return this.settings;
+  }
 }
+
+export default Settings;
